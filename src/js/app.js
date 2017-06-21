8583852378af325bf76737b386f64e3aba97adde
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2017-06-20 22:18:38 -0700

    reset

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,21 @@
-// JS Goes here - ES6 supported
+import auth0 from "auth0-js";
+// auth0 code
+window.addEventListener("load", function() {
+
+  var webAuth = new auth0.WebAuth({
+    domain: "bdougie.auth0.com",
+    clientID: "Y7kZiP2YXhkIe6AsO84r4GDSW3QCnnye",
+    redirectUri: window.location.href,
+    audience: "https://bdougie.auth0.com/userinfo",
+    responseType: "token id_token",
+    scope: "openid"
+  });
+
+  var loginBtn = document.getElementById("btn-login");
+
+  loginBtn.addEventListener("click", function(e) {
+    e.preventDefault();
+    webAuth.authorize();
+  });
+
+});
